0bac55679572df8ff0a4541fc6ef0f9754896bc1
miuguelt <miuguelt@gmail.com>
2024-01-17 18:19:57 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 class Config:
     SECRET_KEY = '1234'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:2hefbFfDEc-HFd1fhc2DhBCCgh-HCB65@monorail.proxy.rlwy.net:20020/Libreria1'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:2hefbFfDEc-HFd1fhc2DhBCCgh-HCB65@monorail.proxy.rlwy.net:20020/Libreria1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
