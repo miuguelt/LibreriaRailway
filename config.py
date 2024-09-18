db67f03d09484d31412afe41c6fcaa2af27c04e4
miuguelt <miuguelt@gmail.com>
2024-09-18 14:32:51 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:2hefbFfDEc-HFd1fhc2DhBCCgh-HCB65@monorail.proxy.rlwy.net:20020/Libreria1'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ChJlGgzZAAyqfGXZSjKKaqiqsFvvrOBO@junction.proxy.rlwy.net:39255/libreria'
     #SQLALCHEMY_DATABASE_URI = 'mysql+mysqlclient://root:2hefbFfDEc-HFd1fhc2DhBCCgh-HCB65@monorail.proxy.rlwy.net:20020/Libreria1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
